dbc4b9f71f8fd6b563063b8fe1e8254e9ed670c3
yolisses <iamurise@gmail.com>
2023-10-11 09:56:26 -0300

    add checkBox

---
+++ src/checkBox.tsx
@@ -0,0 +1,22 @@
+import CheckedFalse from "./assets/check-false.svg";
+import CheckedTrue from "./assets/check-true.svg";
+
+type Props = {
+  checked: boolean;
+  onClick: () => void;
+};
+
+export function CheckBox({ checked, onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        padding: 0,
+        height: "1em",
+        cursor: "pointer",
+      }}
+    >
+      <img src={checked ? CheckedTrue : CheckedFalse} />
+    </button>
+  );
+}
--- src/techItem.tsx
+++ src/techItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { CheckBox } from "./checkBox";
 import { Tech } from "./tech";
 import { useTechs } from "./techsContext";
 
@@ -8,13 +10,22 @@ type Props = {
 export function TechItem({ tech }: Props) {
   const { removeTech } = useTechs();
 
+  const [finished, setFinished] = useState(false);
+
   function handleClick() {
     removeTech(tech.id);
   }
 
+  function handleCheckClick() {
+    setFinished((old) => !old);
+  }
+
   return (
-    <div className="bg-gray-500 flex-row p-1 rounded-0_5">
-      {tech.name}
+    <div className="bg-gray-500 flex-row rounded-0_5 gap-1 p-1">
+      <CheckBox checked={!finished} onClick={handleCheckClick}></CheckBox>
+      <div style={{ textDecoration: !finished ? "line-through" : "" }}>
+        {tech.name}
+      </div>
       <button className="ml-auto" onClick={handleClick}>
         delete
       </button>
